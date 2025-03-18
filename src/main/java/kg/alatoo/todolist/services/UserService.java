744790ae1e3f69cee0946986e5bcb9f732d439d0
Bermet <iskenderova.bermet19@gmail.com>
2025-03-19 02:10:02 +0600

    Created UserService

--- src/main/java/kg/alatoo/todolist/services/UserService.java
+++ src/main/java/kg/alatoo/todolist/services/UserService.java
@@ -1,4 +1,39 @@
 package kg.alatoo.todolist.services;
 
+import kg.alatoo.todolist.entities.User;
+import kg.alatoo.todolist.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
